4553e50b265cba5c852a37b33337136ec1ff7ff4
Tung Nguyen <nguyenthanhtungbk96@gmail.com>
2022-10-25 15:17:10 +0700

    :lipstick: Vue > Lesson 2

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: "Shoes",
-            description: "It's the description for this product."
+            description: "It's the description for this product.",
+            image: './assets/images/green-shoe.png',
+            link: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
         }
     }
 })
