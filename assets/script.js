0a3e1fbc9bd8171f2ea76b4c22398a82b71d48ea
Tung Nguyen <nguyenthanhtungbk96@gmail.com>
2022-10-25 16:03:02 +0700

    :lipstick: Vue > Lesson 5

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
             product: "Shoes",
             description: "It's the description for this product.",
             image: './assets/images/green-shoe.png',
-            onSale: true,
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id:1000,  color:"green"},
+                {id:1001,  color:"blue"}
+            ],
+            sizes: [
+                {size: 20, people: "kid"},
+                {size: 32, people: "women"},
+                {size: 42, people: "men"}
+            ]
         }
     }
 })
