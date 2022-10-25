405cc061fd1d74cbc2a1886774f1192e3719ce14
Tung Nguyen <nguyenthanhtungbk96@gmail.com>
2022-10-25 16:35:38 +0700

    :lipstick: Vue > Lesson 6: Event Handling

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,28 @@ const app = Vue.createApp({
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id:1000,  color:"green"},
-                {id:1001,  color:"blue"}
+                {id:1000,  color:"green", image:"./assets/images/green-shoe.png"},
+                {id:1001,  color:"blue", image: "./assets/images/blue-shoe.png"}
             ],
             sizes: [
                 {size: 20, people: "kid"},
                 {size: 32, people: "women"},
                 {size: 42, people: "men"}
-            ]
+            ],
+            cart:0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart++;
+        },
+        removeToCart() {
+            if (this.cart >0) {
+                this.cart--;
+            }
+        },
+        updateImage(image) {
+            this.image = image;
         }
     }
 })
